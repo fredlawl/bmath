12abcd148d18876199879a999117858a97f0fe96
Frederick Lawler <fred@fredlawl.com>
2022-11-25 16:02:07 -0600

    Change str_hex_to_uint64 to not need pow()
    Change str_hex_to_uint64 to not need pow().

--- src/conversions.h
+++ src/conversions.h
@@ -3,50 +3,46 @@
 
 #include <stdint.h>
 #include <ctype.h>
-#include <math.h>
 
 #include "shim.h"
 
 static bool str_hex_to_uint64(const char *input, uint64_t *result)
 {
 	char current_char;
-	uint64_t exp;
 
-	size_t input_length = strlen(input) - 1;
+	size_t input_length = strlen(input);
 	const char *input_start = input;
 	const char *input_end = input + input_length;
-	int counter = 0;
 
 	if (*input_start++ != '0')
 		return false;
 
-	if (*input_start != 'x' && *input_start != 'X')
+	if (!(*input_start == 'x' || *input_start == 'X'))
 		return false;
 
+	input_start++;
+
 	*result = 0;
-	while (true) {
-		current_char = *input_end--;
-		if (current_char == 'x' || current_char == 'X')
-			break;
+	do {
+		uint64_t temp;
+		current_char = *input_start++;
 
 		if (!__ishexnumber(current_char))
 			return false;
 
-		exp = (uint64_t) pow(16, counter);
-		counter++;
-
+		*result = *result << 4;
 		if (isdigit(current_char)) {
-			*result += (uint64_t) (current_char - '0') * exp;
-			continue;
+			temp = current_char - '0';
+		} else {
+			current_char = (current_char >= 'A' &&
+					current_char <= 'F') ?
+				  	current_char :
+				    	(char) (current_char - 32);
+			temp = (current_char - 'A') + 10;
 		}
-
-		current_char = (current_char >= 'A' &&
-				current_char <= 'F') ?
-				current_char :
-                               (char) (current_char - 32);
-
-		*result += ((current_char - 'A') + 10) * exp;
-	}
+				
+		*result = *result + temp;
+	} while (input_start != input_end);
 
 	return true;
 }
