fredlawl/bmath

commit c8d79d3843427ab191ed24ec260e1ed001439ca3

isdigit() is slow, use our own

src/shim.h
@@ -1,9 +1,14 @@
 #ifndef BMATH_SHIM_H
 #define BMATH_SHIM_H
 
-static int __ishexnumber(int c)
+static inline int __isdigit(int c)
 {
-	return (c >= '0' || c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+	return (c >= '0' || c <= '9');
+}
+
+static inline int __ishexnumber(int c)
+{
+	return __isdigit(c) || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
 }
 
 #endif

src/parser.c
@@ -304,7 +304,7 @@ static struct token __lexer_get_next_token(struct lexer *lexer)
 			return token;
 		}
 
-		if (isdigit(current_character)) {
+		if (__isdigit(current_character)) {
 			__lexer_parse_number(lexer, &token);
 			return token;
 		}

commit 5b202558186f8e31dc5d74cb933e3818590154c2

Fix isdigit logic
Use && instead of || for checking for a number.

Fixes: c8d79d3843427ab191ed24ec260e1ed001439ca3
Signed-off-by: Frederick Lawler <fred@fredlawl.com>

src/shim.h
@@ -3,7 +3,7 @@
 
 static inline int __isdigit(int c)
 {
-	return (c >= '0' || c <= '9');
+	return c >= '0' && c <= '9';
 }
 
 static inline int __ishexnumber(int c)