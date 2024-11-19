1e066ca511c29293a9bee824f70cb66d03edb5d1
Frederick Lawler <me@fred.software>
2024-11-18 18:29:46 -0600

    use build-id for debug symbols

--- build-release.sh
+++ build-release.sh
@@ -7,47 +7,47 @@ VERSION=$(grep -Po "^project\(.*VERSION\s\K[0-9]+\.[0-9]+\.[0-9]+" CMakeLists.tx
 PKG=$(realpath "bmath_$VERSION"*.deb)
 PKG_DBG=$(realpath "bmath-dbg_$VERSION"*.deb)
 BINARY="bin/usr/bin/bmath"
-DEBUG_BINARY="dbg/usr/lib/debug/usr/bin/bmath.debug"
+BUILD_ID=$(readelf -n "$BINARY" | grep -Po 'Build ID:\s\K[0-9a-zA-Z]+')
+BUILD_ID_START=${BUILD_ID:0:2}
+BUILD_ID=${BUILD_ID:2}
+DEBUG_BINARY="dbg/usr/lib/debug/.build-id/$BUILD_ID_START/$BUILD_ID.debug"
 
 rm -rf dbg
-mkdir -p dbg/usr/lib/debug/usr/bin
+mkdir -p $(dirname "$DEBUG_BINARY")
 
-objcopy --only-keep-debug $BINARY $DEBUG_BINARY
-strip --strip-debug --strip-unneeded $BINARY
-objcopy --add-gnu-debuglink=$DEBUG_BINARY $BINARY
-# https://github.com/Debian/debhelper/blob/master/dh_strip#L291
-# just awk out the build-id to have debug symbols across multiple versions, but gdb still works
-# as is.
-rm -f $PKG $PKG_DBG
+objcopy --only-keep-debug "$BINARY" "$DEBUG_BINARY"
+strip --strip-debug --strip-unneeded "$BINARY"
+objcopy --add-gnu-debuglink="$DEBUG_BINARY" "$BINARY"
+chmod -x "$DEBUG_BINARY"
+rm -f "$PKG" "$PKG_DBG"
 
 fpm \
-    -s dir \
-    -t deb \
-    -v $VERSION \
-    -n bmath \
-    -a amd64 \
-    -m "Frederick Lawler <me@fred.software>" \
-    --url "https://github.com/fredlawl/bmath" \
-    --description "64bit big-endian bitwise operation calculator" \
-    --license "MIT" \
-    -d "libncurses6" \
-    -d "libc6 >= 2.15" \
-    -d "libtinfo6" \
-    -C bin \
-    .
+  -s dir \
+  -t deb \
+  -v $VERSION \
+  -n bmath \
+  -a amd64 \
+  -m "Frederick Lawler <me@fred.software>" \
+  --url "https://github.com/fredlawl/bmath" \
+  --description "64bit big-endian bitwise operation calculator" \
+  --license "MIT" \
+  -d "libncurses6" \
+  -d "libc6 >= 2.15" \
+  -d "libtinfo6" \
+  -C bin \
+  .
 
 fpm \
-    -s dir \
-    -t deb \
-    -v $VERSION \
-    -n bmath-dbg \
-    -a amd64 \
-    -m "Frederick Lawler <me@fred.software>" \
-    --url "https://github.com/fredlawl/bmath" \
-    --description "64bit big-endian bitwise operation calculator" \
-    --license "MIT" \
-    -d "bmath = $VERSION" \
-    --deb-priority optional \
-    -C dbg \
-    .
-
+  -s dir \
+  -t deb \
+  -v $VERSION \
+  -n bmath-dbg \
+  -a amd64 \
+  -m "Frederick Lawler <me@fred.software>" \
+  --url "https://github.com/fredlawl/bmath" \
+  --description "64bit big-endian bitwise operation calculator" \
+  --license "MIT" \
+  -d "bmath = $VERSION" \
+  --deb-priority optional \
+  -C dbg \
+  .
